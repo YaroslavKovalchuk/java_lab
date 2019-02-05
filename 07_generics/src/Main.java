c5248400f265b89974b6ac6bdd86ec9f889c82da
YaroslavKovalchuk <35632982+YaroslavKovalchuk@users.noreply.github.com>
2019-02-06 00:32:29 +0200

    Update Main.java

--- 07_generics/src/Main.java
+++ 07_generics/src/Main.java
@@ -1,6 +1,9 @@
 import task_1_shipWithDroids.ConcreteDroid;
 import task_1_shipWithDroids.Droid;
 import task_1_shipWithDroids.Ship;
+import priorityQueue.Droid;
+import priorityQueue.DroidsQueue;
+
 
 public class Main {
 
@@ -10,11 +13,27 @@ public class Main {
 
         Droid petro = new ConcreteDroid();
         petro.setName("petro");
+        
+        Droid andriy = new Droid();
+        andriy.setName("andriy");
+
+        Droid denys = new Droid();
+        denys.setName("denys");
 
         Ship<Droid> ship = new Ship<>();
         ship.put(ivan);
         ship.put(petro);
         System.out.println(ship.get(0).getName());
         System.out.println(ship.get(2).getName());
+    
+
+        DroidsQueue<Droid> droidsQueue = new DroidsQueue<>();
+        droidsQueue.add(ivan);
+        droidsQueue.add(andriy);
+        droidsQueue.add(denys);
+
+        for ( int i = 0; i < droidsQueue.getSize(); i ++){
+            System.out.println(droidsQueue.get(i));
+        }
     }
 }
